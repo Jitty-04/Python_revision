19b799ee8ef7fced4894c7576571f6b6e3bdf05e
jitty <jittysjohn1997@gmail.com>
2025-02-18 12:01:58 +0530

    17feb

---
+++ extrat.py
@@ -0,0 +1,7 @@
+text="hjjkkl798090nkjkl787jkjk"
+
+result=''
+for i in text:
+    if i.isdigit():
+        result+=i
+print(result)
---
+++ specialchar.py
@@ -0,0 +1,2 @@
+string=input("'enter the string:").strip()
+print(False if string.isalnum() else True )
