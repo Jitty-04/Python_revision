4b3da3e0572d1d44d45bc96f4bc5a75073d7c851
jitty <jittysjohn1997@gmail.com>
2025-02-26 18:59:46 +0530

    25feb

---
+++ functions/intro.py
@@ -0,0 +1,21 @@
+"""
+print()
+len()
+input()
+range()
+sum()
+"""
+
+print("hello gud evng")
+
+def greet():  # function header
+    print("hello gud evng")  # function body
+
+greet()  # function caller
+
+def addition(a, b):
+    print(a + b)
+
+addition(8, 7)
+addition(10, 15)
+addition(-8, 7)
